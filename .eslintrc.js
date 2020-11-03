46ea29af210f9384727f80e1dda3af092a04ae52
Kupyrev Alexander <alexander.kupyrev@pladform.ru>
2020-11-03 16:53:53 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks', 'react-app', 'prettier', 'prettier/react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.tsx', '.ts', '.js'] }],
+    'import/extensions': [
+      'warn',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'webpack.config.js',
+      },
+    },
+  },
+};
---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './Header.module.scss';
+
+const Header = () => {
+  return <h1 className={s.header}>Header</h1>;
+};
+
+export default Header;
