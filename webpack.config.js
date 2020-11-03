31184d1582f67f71e4cd3dd7d7fb74bb20bf1ecd
Kupyrev Alexander <alexander.kupyrev@pladform.ru>
2020-11-04 02:20:32 +0300

    +

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  watch: true,
+  watchOptions: {
+  ignored: /node_modules/,
+  poll: 1000 // Check for changes every second
+  },
   module: {
     rules: [
       {
