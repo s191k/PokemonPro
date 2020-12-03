03bc6dc801a3c7bb9efb0418b8129da805dcaf69
Kupyrev Alexander <alexander.kupyrev@pladform.ru>
2020-12-03 21:28:31 +0300

    'add'

--- src/server/server.js
+++ src/server/server.js
@@ -1,31 +1,50 @@
-'use strict';
+import Hapi from '@hapi/hapi';
+import ReactDom from 'react-dom/server'
+import NotFoundPage from '../pages/NotFoundPage';
+import {setPath} from 'hookrouter';
+import React from 'react';
+import App from '../App';
 
-const Hapi = require('@hapi/hapi');
+import fs from 'fs';
+import path from 'path';
+import handlebars from 'handlebars';
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  await server.register(require('@hapi/inert'));
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
+  server.route({
+    method: 'GET',
+    path: '/main.js',
+    handler: (request, h) => h.file(path.join(process.cwd(), 'dist', 'main.js'))
+  });
 
-            return 'Hello World!';
-        }
-    });
+  server.route({
+    method: 'GET',
+    path: '/{any*}',
+    handler: (request, h) => {
+        setPath(request.path);
+        const pathIndexHTML = path.join(process.cwd(),'dist','index.html');
+        const template = handlebars.compile(fs.readFileSync(pathIndexHTML,'utf8'));
+        const result = ReactDom.renderToString(<App/>);
+        const page = template({
+            content:result
+        });
+        return page;
+    },
+  });
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
